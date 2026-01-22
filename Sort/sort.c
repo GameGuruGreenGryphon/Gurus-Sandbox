930620255786d364cc26de45036a4fbba4f8c0f8
guru <gamegurukid@gmail.com>
2026-01-22 16:16:18 -0700

    Attempting Quicksort

--- Sort/sort.c
+++ Sort/sort.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
 
 void echo(char* string) {
 	printf("%s\n", string);
@@ -107,6 +108,77 @@ int bogosort(int* list, int length) {
 	}
 
 	return steps;
+}
+
+/*
+Can you do
+(1) A merge sort (with recursion)
+(2) A quick sort (with recursion)
+on arrays with sort keys such as double or strings to your liking?
+Nothing is gained if you don’t know whether your programs are correct. Therefore, can you
+provide a simple test routine that checks whether the resulting array really is sorted?
+This test routine should just scan once through the array and should be much, much faster than
+your sorting algorithms.
+
+-------
+
+What's quick sort?
+
+https://en.wikipedia.org/wiki/Merge_sort
+
+General-purpose sorting algorithm
+Made in 1959 by Tony Hoare and published in 1961
+Slightly faster than merge sort
+Quicksort is a divide-and-conquer algorithm
+
+1. If the current range has two or less elements, return
+2. Pick a pivot point
+3. Sort each element and compare it to the pivot point
+	If the element is greater than the pivot, put it on the right
+	If not, put it on the left
+	We do not care about how MUCH greater or less than it is compared to the pivot point
+4. Call the function again on each sub-range
+
+If we had a list of 11 elements, 0 through 10...
+The middle index would be... 5 with a value of 4, ideally
+0 - 4  is less than 5
+6 - 10 is greater than 5
+
+[0] = 7		X
+[1] = 2
+[2] = 10	X
+[3] = 1
+[4] = 8		X
+[5] = 5		X	Pivot
+[6] = 11
+[7] = 3		X
+[8] = 4		X
+[9] = 6
+[10] = 9
+
+Divide the subranges up between 0-4 and 6-10
+Each subrange has an equal amount of numbers in the wrong place, 3
+Swap 7 with 3, 10 with 4, and 8 with 6
+
+*/
+
+void quicksort(int* list, int length, int start, int end) {
+
+	if (length <= 2) {return;}
+
+	int pivot = (int) floor(length) / 2;
+
+	for (int i = start; i < end; i++) {
+
+		// If index is less than pivot and value is greater than pivot
+		if (i < pivot && list[i] > pivot) {
+
+			quicksort(list, end - start, newStart, newEnd);
+		} else {
+
+			quicksort(list, end - start, newStart, newEnd);
+		}
+	}
 }
 
 void print_list(int* list, int length) {
