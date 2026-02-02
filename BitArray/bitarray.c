cf4c2de24efaad0d3adf8cb04091872c911fd3c6
guru <gamegurukid@gmail.com>
2026-02-02 09:54:10 -0700

    Added comments and such

--- BitArray/bitarray.c
+++ BitArray/bitarray.c
@@ -8,28 +8,43 @@ struct bit_array {
 	uint64_t* inner;
 };
 
-// CALLOC!!!!
-uint64_t* AllocateArray(unsigned int bit_length) {
-	unsigned int size = bit_length / 64;
-	uint64_t* new = calloc(size, sizeof(uint64_t));
-	return new;
+int count = 0;
+void debug(char* message) {
+	printf("%s%d%s%s%s\n", "[", count, "] <", message, ">");
+	count++;
 }
 
 // Handles the struct of the array
 struct bit_array MakeBitArray(unsigned int bit_length) {
-	unsigned int bit_indices = bit_length - 1;
-	unsigned int array_indices = bit_length / 64;
-	uint64_t* inner = AllocateArray(bit_length);
 
-	struct bit_array new = { bit_indices, array_indices, inner };
+	// Indexes are 0-based
+	unsigned int bit_indices = bit_length - 1;
+
+	// b / 64 is always truncated
+	// if b = 0 - 63, b/64 = 0
+	// if b = 64, b/64 = 1, but 64 bits can be stored in one element
+	uint32_t to_alloc = (bit_length / 64) + ((bit_length % 64) == 0);
+
+	// Actually allocate the memory
+	uint64_t* inner = calloc(to_alloc, sizeof(uint64_t));
+
+	// Structs!
+	struct bit_array new = { bit_indices, to_alloc, inner };
 	return new;
 }
 
 // Returns 1 or 0
 unsigned int GetBit(struct bit_array array, unsigned int index) {
+
+	// We don't do the fancy math from MakeBitArray() in here because this is 0-based
 	int array_elem_index = index / 64;
+
+	// Don't index bits outside of the 64 bit range
 	int index_within_elem = index % 64;
+
 	uint64_t array_elem = array.inner[array_elem_index];
+
+	// Bit shift uint64 right until the least significant bit is the bit we want to retrieve
 	uint64_t mask = 1;
 	return (array_elem >> index_within_elem) & mask;
 }
@@ -37,15 +52,17 @@ unsigned int GetBit(struct bit_array array, unsigned int index) {
 // Sets a bit to 1 or 0
 void SetBit(struct bit_array array, unsigned int index, unsigned int bool) {
 
+	debug("Setbit | Check");
 	if (index > array.bit_indices) {
 		array.bit_indices = index;
 	}
 
+	debug("Setbit | Malloc");
 	// If index is beyond actual allocated memory
 	if (index > ( (array.array_indices + 1) * 64)) {
 
 		// downloadmoreram.com
-		uint64_t* new = AllocateArray(index);
+		uint64_t* new = calloc(index / 64, sizeof(uint64_t));
 
 		// Copy data to new memory location
 		for (unsigned int i = 0; i <= array.array_indices; i++) {
@@ -62,6 +79,7 @@ void SetBit(struct bit_array array, unsigned int index, unsigned int bool) {
 	int array_elem_index = index / 64;
 	int index_within_elem = index % 64;
 
+	debug("Setbit | Bool set");
 	if (bool) {
 		array.inner[array_elem_index] |= (1 << index_within_elem);
 	} else {
@@ -88,21 +106,52 @@ void SetBitRange(struct bit_array changed_array, unsigned int index, struct bit_
 	}
 }
 
-void PrintBitArray(struct bit_array array) {
-	for (unsigned int i = 0; i <= array.bit_indices; i++) {
-		if (GetBit(array, i)) {
-			printf("%c", '1');
-		} else {
-			printf("%c", '0');
+// Converts scalar values into bit arrays
+struct bit_array ToBitArray(uint64_t number) {
+
+	uint64_t length = 0;
+
+	while (length < 65) {
+		uint64_t mask = ~0;
+		mask = mask << length;
+
+		if ( (number & ~mask) == number) {
+			break;
 		}
+
+		length++;
 	}
+
+	struct bit_array new = MakeBitArray(length);
+
+	for (int i = 0; i <= length; i++) {
+		SetBit(new, i, (number >> i) & 1);
+	}
+
+	return new;
+}
+
+void PrintBitArray(struct bit_array array) {
+	for (int i = array.bit_indices; i >= 0; i--) {
+		printf("%d", GetBit(array, i));
+	}
+
 	printf("\n");
 }
 
 int main() {
-	struct bit_array epic = MakeBitArray(100);
+	struct bit_array epic = MakeBitArray(64);
 	PrintBitArray(epic);
 
 	SetBit(epic, 5, 1);
 	PrintBitArray(epic);
+
+	struct bit_array awesome = ToBitArray(75);
+	PrintBitArray(awesome);
+
+	SetBitRange(epic, 0, awesome);
+	PrintBitArray(epic);
+	free(awesome.inner);
+
+	free(epic.inner);
 }
