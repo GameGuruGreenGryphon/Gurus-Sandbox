b95d3f276e59888fe61a4ea8402024a5de31ffe0
guru <gamegurukid@gmail.com>
2026-02-04 18:28:23 -0700

    Not quite done yet

--- LinkedList/linkedlist.c
+++ LinkedList/linkedlist.c
@@ -8,6 +8,7 @@ struct node {
 
 #define MAXLIST 100
 char ENDLIST = '\0';
+char STARTLIST = ~0;
 
 struct node* MakeNode(struct node* next, char data) {
 	struct node *newNode = malloc(sizeof(struct node));
@@ -16,46 +17,63 @@ struct node* MakeNode(struct node* next, char data) {
 	return newNode;
 }
 
-struct node* InitList(char data) {
-	struct node* tail = MakeNode(NULL, '\0');
-	struct node* head = MakeNode(tail, data);
+struct node* InitList() {
+	struct node* tail = MakeNode(NULL, ENDLIST);
+	struct node* head = MakeNode(tail, STARTLIST);
 	return head;
 }
 
 int iter = 0;
 
 void PrintList(struct node* node) {
-	printf("[%d]: %c", iter++, node->data);
-	if (node->data != '\0') {
+	if (node->data != STARTLIST) {
+		printf("[%d]: %c\n", iter++, node->data);
+	}
+
+	if (node->next->data != ENDLIST) {
 		PrintList(node->next);
+	} else {
+		iter = 0;
 	}
 }
 
-// Traverse list until we find penultimate node
 // The true last node is the terminator node, which points to null
 // Deltas below 0 will traverse the nodes until the terminator is found
 // Deltas above 0 will traverse until it finds the node past delta
 struct node* FindNode(struct node* startNode, int delta) {
 
 	int count = 0;
-	struct node* pen = startNode;
-	while (pen->next->data != '\0' && delta-- != 0) {
-		pen = startNode->next;
+	struct node* node = startNode;
 
-		if (count++ > MAXLIST) {
-			return NULL;
+	if (delta < 0) {
+		// Find penultimate list item
+		while (node->next->data != ENDLIST && count++ < MAXLIST) {
+			node = node->next;
+		}
+	} else {
+		// Find list terminator
+		while (node->data != ENDLIST && delta-- != 0 && count++ < MAXLIST) {
+			node = node->next;
 		}
 	}
-	return pen;
+	return node;
 }
 
-void DeleteNextNode(struct node* prevNode) {
-	struct node* nextNode = FindNode(prevNode, 1);
+//
+int DeleteNextNode(struct node* node, int delta) {
+	struct node* nextNode = FindNode(node, delta);
+	if (nextNode->data == ENDLIST) {
+		printf("Fail 1");
+		return 0;
+	}
+
 	struct node* afterThat = FindNode(nextNode, 1);
 
-	prevNode->next = afterThat;
+	node->next = afterThat;
 
 	free(nextNode);
+	
+	return 1;
 }
 
 void FreeList(struct node* head) {
@@ -64,16 +82,10 @@ void FreeList(struct node* head) {
 
 	while (1) {
 		struct node* nextNode = FindNode(reaper, 1);
-		// nextNode returns itself if the next node is the terminator
+		free(reaper);
 		if (reaper == nextNode) {
-			// free terminator
-			free(reaper->next);
-			// free the reaper
-			free(reaper);
 			break;
 		}
-
-		free(reaper);
 		reaper = nextNode;
 	}
 }
@@ -88,15 +100,34 @@ struct node* InsertNode(struct node* startNode, char data, int delta) {
 }
 
 int main() {
-	struct node* head = InitList('h');
+	struct node* head = InitList();
+	PrintList(head);
+	printf("\n");
+
+	InsertNode(head, 'h', -1);
+	PrintList(head);
+	printf("\n");
 
 	for (int i = 0; i < 10; i++) {
 		InsertNode(head, 'e', -1);
 	}
 
-	InsertNode(head, 'l', 5);
-	InsertNode(head, 'l', 5);
+	PrintList(head);
+	printf("\n");
 
+	InsertNode(head, 'o', 2);
+	InsertNode(head, 'l', 2);
+	InsertNode(head, 'l', 2);
+	PrintList(head);
+	printf("\n");
+	
+	printf("Testing DeleteNextNode()");
+	DeleteNextNode(head, -1);
+	PrintList(head);
+	printf("\n");
+
+	printf("Testing DeleteNextNode()");
+	DeleteNextNode(head, -1);
 	PrintList(head);
 
 	FreeList(head);
