3d3c7f7ac61021a483caa0507698af4a017b39e1
guru <gamegurukid@gmail.com>
2026-02-05 10:50:29 -0700

    Does this already work??

--- BinaryTree/binarytree.c
+++ BinaryTree/binarytree.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXLIST 127
+
 struct node {
-	char data;
+	int data;
 	struct node* left;
 	struct node* right;
 };
 
-struct node* MakeNode(struct node* left, struct node* right, char data) {
+struct node* MakeNode(struct node* left, struct node* right, int data) {
 	struct node *newNode = malloc(sizeof(struct node));
 	newNode->data = data;
 	newNode->left = left;
@@ -16,21 +18,65 @@ struct node* MakeNode(struct node* left, struct node* right, char data) {
 }
 
 void PrintNode(struct node* node, char* name) {
-	printf("[%s]\tdata: %c\taddress: %p\tleft: %p\n right: %p", name, node->data, node, node->left, node->right);
+	printf("[%s]\tdata: %3d\taddress: %14p\tleft: %14p\t right: %14p\n", name, node->data, node, node->left, node->right);
 }
 
-void PrintList(struct node* node) {
-	while (node) {
+void PrintTree(struct node* node) {
+	if (node) {
 		PrintNode(node, "PrintList");
-		PrintList(node->left);
-		PrintList(node->right);
+		PrintTree(node->left);
+		PrintTree(node->right);
 	}
 }
 
 void FreeTree(struct node* node) {
+	if (node) {
+		FreeTree(node->left);
+		FreeTree(node->right);
+		free(node);
+	}
 }
 
+struct node* InsertNode(struct node* root, int data) {
+	struct node* node = root;
+	struct node* prevNode = node;
+	while (node) {
+		prevNode = node;
+		if (data > node->data) {
+			node = node->right;
+		} else {
+			node = node->left;
+		}
+	}
+
+	if (data > prevNode->data) {
+		return prevNode->right = MakeNode(NULL, NULL, data);
+	} else {
+		return prevNode->left = MakeNode(NULL, NULL, data);
+	}
+
+}
 
 int main() {
-	struct node* root = MakeNode(NULL, NULL, '\0');
+	struct node* root = MakeNode(NULL, NULL, MAXLIST / 2);
+	PrintTree(root);
+	printf("\n");
+
+	InsertNode(root, 5);
+	PrintTree(root);
+	printf("\n");
+
+	InsertNode(root, 10);
+	InsertNode(root, 37);
+	InsertNode(root, 100);
+	InsertNode(root, 1);
+	InsertNode(root, 4);
+	InsertNode(root, 8);
+	InsertNode(root, 16);
+	InsertNode(root, 32);
+	InsertNode(root, 64);
+	PrintTree(root);
+	printf("\n");
+
+	FreeTree(root);
 }
